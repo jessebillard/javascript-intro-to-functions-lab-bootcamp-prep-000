c0fdc68066f3bfdfbf7ce7b988eb110d8af054ff
Jesse Billard <jessebillard@mac.com>
2018-01-18 19:12:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,18 +23,7 @@ function sayHitToGrandma(string) {
     }
   }
   
-}
 
 
-function sayHiToGrandma(string) {
- if (string.toLowerCase() === string) {
- return "I can\'t hear you!"
- }
-}
 
-function sayHiToGrandma(string) {
-  if (string.toUpperCase() === string) {
-  return "YES INDEED!"
-  }
-}
 
