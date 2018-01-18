6aa80ab962950bf4ff9b6af570c9f90f6013df2f
Jesse Billard <jessebillard@mac.com>
2018-01-18 19:19:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string) {
     else if (string.toUpperCase() === string) {
       return "YES INDEED!"
     }
-    else (string("I love you, Grandma.") === string) {
+    else (string === string) {
       return "I love you, too."
     }
   }
