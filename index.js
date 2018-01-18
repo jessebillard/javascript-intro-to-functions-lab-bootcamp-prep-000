6c453aa89d6fd53dd128e9344b603699f96a0a8e
Jesse Billard <jessebillard@mac.com>
2018-01-18 19:12:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string) {
   console.log('hello')
 }
 
-function sayHitToGrandma(string) {
+function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can\'t hear you!"
   }
