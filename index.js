0e7c25425b8ba6703c72f75816c2ab5bf8242486
Jesse Billard <jessebillard@mac.com>
2018-01-18 19:17:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function sayHiToGrandma(string) {
       return "YES INDEED!"
     }
     else if (string("I love you, Grandma.") === string) {
-      return "I love you too"
+      return "I love you, too."
     }
   }
   
